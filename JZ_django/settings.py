a11f351e8f95f7adfaf56ccabcfdc4a9e1ef9643
Tom <andypandy12345@yahoo.com>
2016-11-28 15:12:39 +0200

    my first commit

--- JZ_django/settings.py
+++ JZ_django/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '8*krrl)btzja*+2xi^)z89(4)gljzh#ap*&x-fln@)t5moq*g3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
